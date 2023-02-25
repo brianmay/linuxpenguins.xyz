40a5e0efb6cd4c4fee1bc8879bd29faa9986ef12
Brian May <brian@linuxpenguins.xyz>
2023-02-26 10:57:22 +1100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -ex
+
+if [ -n "$(git status --porcelain)" ]; then
+    echo "Working directory is not clean" >&2
+    exit 1
+fi
+
+VCS_REF="$(git rev-parse HEAD)"
+BUILD_DATE="$(date --iso-8601=seconds)"
+sed -i 's,SHA,'"$VCS_REF"',' html/index.html
+sed -i 's,REF,'"$BUILD_DATE"',' html/index.html
+rsync -v html/* brian@master.linuxpenguins.xyz:/var/www/html
+git checkout html/index.html
